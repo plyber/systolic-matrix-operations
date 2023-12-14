9ed2157fd5c9fdd005eb42d4aa9ad314c588c104
Oliviu Dienes <76002464+plyber@users.noreply.github.com>
2023-12-14 13:14:10 +0200

    Added runtime variable, can be adjusted to see partial results; Enhanced systolic characteristics

--- MatxMat.py
+++ MatxMat.py
@@ -31,41 +31,30 @@ class Processor:
 
 
 def systolic_matrix_multiplication(A, B):
+
+    runtime=5;
+
     a_rows, a_cols = A.shape
-    b_cols = B.shape[1]
+    b_cols, b_rows = B.shape
     C = np.zeros((a_rows, b_cols))
 
     processors = [[Processor() for _ in range(b_cols)] for _ in range(a_rows)]
 
-    for step in range(a_rows + a_cols - 1):
+    for step in range(a_rows + a_cols + runtime):
         print(f'\n--- TIME STEP {step} ---')
         # incarca valorile in procesoare
-        for i in range(a_rows):
-            a_index = step - i
-            if 0 <= a_index < a_cols:
-                print(f'PROCESSOR {i, 0} LOAD A: {A[i, a_index]}')
-                processors[i][0].load_a(A[i, a_index])
-
-        for j in range(b_cols):
-            b_index = step - j
-            if 0 <= b_index < a_cols:
-                print(f'PROCESSOR {0, j} LOAD B: {B[b_index, j]}')
-                processors[0][j].load_b(B[b_index, j])
-
-        # calculeaza, paseaza si reseteaza pentru urmatoarea iteratie
         for i in reversed(range(a_rows)):
             for j in reversed(range(b_cols)):
+                processors[i][j].a = processors[i][j - 1].a if j > 0 else A[i, step - i] if 0 <= step - i < a_rows else 0
+                processors[i][j].b = processors[i - 1][j].b if i > 0 else B[step - j, j] if 0 <= step - j < b_cols else 0
                 processors[i][j].compute()
-                print(f'PROCESSOR {i, j} COMPUTE {processors[i][j].a} * {processors[i][j].b} = {processors[i][j].c} ')
-                if j < b_cols - 1:
-                    a_to_pass = processors[i][j].pass_a_east()
-                    print(f'PROCESSOR {i, j} PASS A {a_to_pass}')
-                    processors[i][j + 1].load_a(a_to_pass)
-                if i < a_rows - 1:
-                    b_to_pass = processors[i][j].pass_b_south()
-                    print(f'PROCESSOR {i, j} PASS B {b_to_pass}')
-                    processors[i + 1][j].load_b(b_to_pass)
 
+        # afiseaza date
+        for i in range(a_rows):
+            for j in range(b_cols):
+                print(f'PROCESSOR {i, j} COMPUTE: + {processors[i][j].a} * {processors[i][j].b} = {processors[i][j].c}')
+
+    #colecteaza datele
     for i in range(a_rows):
         for j in range(b_cols):
             C[i, j] = processors[i][j].get_result()
